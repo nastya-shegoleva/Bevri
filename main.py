0410a6ad222df9f161b9df27cfdbd6ae3412e3b1
Nastya <nastshegoleva@yandex.ru>
2024-03-25 20:20:29 +0300

    логотип сайта и навигатор

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def main_page():
+    return render_template('main.html', title='Bevri')
+
+
+if __name__ == '__main__':
+    app.run()
